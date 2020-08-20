c35ef3ffeab658c8c25c9826b666bec36eeb0135
Andrés Torres <towerspro@gmail.com>
2020-08-20 12:42:03 +0200

    refactor: fix dependencies, move test to JUnit5

--- config-server/src/test/java/com/backbase/goldensample/CentralizedConfigServerTests.java
+++ config-server/src/test/java/com/backbase/goldensample/CentralizedConfigServerTests.java
@@ -1,10 +1,11 @@
 package com.siriusxi.cloud.infra.cs;
 
-import org.junit.Assert;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(
     webEnvironment = RANDOM_PORT,
@@ -13,6 +14,6 @@ class CentralizedConfigServerTests {
 
   @Test
   void contextLoads() {
-    Assert.assertTrue(true);
+    Assertions.assertTrue(true);
   }
 }
