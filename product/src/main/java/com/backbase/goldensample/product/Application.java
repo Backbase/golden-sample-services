b7e29e7d4afd0c20f36f044c4c1b7f87f7114a00
jamesv <jamesv@backbase.com>
2020-08-31 13:38:29 +0200

    Changed Docker Image to bootable jar

--- product/src/main/java/com/backbase/goldensample/product/Application.java
+++ product/src/main/java/com/backbase/goldensample/product/Application.java
@@ -3,7 +3,6 @@ package com.backbase.goldensample.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -15,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.backbase.goldensample.product.persistence"})
 @EnableJpaAuditing
 @ComponentScan("com.backbase.goldensample.product")
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
