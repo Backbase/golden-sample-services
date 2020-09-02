f05f9da65c069d61eba32a0fd16f264931d4049a
jamesv <jamesv@backbase.com>
2020-09-02 11:16:18 +0200

    Revert removal of SpringBootServletInitializer

--- review/src/main/java/com/backbase/goldensample/review/Application.java
+++ review/src/main/java/com/backbase/goldensample/review/Application.java
@@ -3,6 +3,7 @@ package com.backbase.goldensample.review;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,7 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.backbase.goldensample.review.persistence"})
 @EnableJpaAuditing
 @ComponentScan("com.backbase.goldensample.review")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
--- store/src/main/java/com/backbase/goldensample/store/Application.java
+++ store/src/main/java/com/backbase/goldensample/store/Application.java
@@ -2,13 +2,14 @@ package com.backbase.goldensample.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan("com.backbase.goldensample.store")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
