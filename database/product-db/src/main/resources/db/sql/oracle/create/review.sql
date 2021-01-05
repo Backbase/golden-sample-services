71a59ff4ee2cfed13b2a2a9ae253a37d64f453cf
Andrés Torres <towerspro@gmail.com>
2021-01-05 13:45:23 +0100

    fix: remove liquibase.properties fix: remove java.version property, SSDK rules fix: remove wrong sql scripts doc: readme updated with the exact folder structure fix: api module parent reverted

--- database/product-db/src/main/resources/db/sql/oracle/create/review.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE SEQUENCE hibernate_sequence START WITH 1 INCREMENT BY 5;
-
-CREATE TABLE product (id NUMBER(38, 0) NOT NULL, create_date TIMESTAMP, name VARCHAR2(255), weight NUMBER(5), CONSTRAINT PK_PRODUCT PRIMARY KEY (id));
-
-CREATE UNIQUE INDEX public.idx_create_date ON public.product(create_date DESC);
-
