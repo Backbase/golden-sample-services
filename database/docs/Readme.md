90094aa3b30f99f373c39a4a4c07d1f8329d92c8
Andrés Torres <towerspro@gmail.com>
2021-01-15 15:52:57 +0100

    fix: enable liquibase in services product and review feat: add remarks to the liquibase scripts to generate comments feat: add SchemaSpy for DB docs autogenerated

---
+++ database/docs/Readme.md
@@ -0,0 +1,11 @@
+# Documentation Generated with SchemaSpy
+
+SchemaSpy is a Java-based tool (requires Java 8 or higher) that analyzes the metadata of a schema in a database 
+and generates a visual representation of it in a browser-displayable format.
+
+[Read more](https://schemaspy.readthedocs.io/en/latest/)
+
+## How to use it
+We have prepared a script to download SchemaSpy, MySQL connector and execute the doc generation. You can use in MacOS/UNIX
+
+    >sh run.sh
--- database/review-db/src/main/resources/db/generated-sql/mysql/create/review.sql
+++ database/review-db/src/main/resources/db/generated-sql/mysql/create/review.sql
@@ -1,7 +1,11 @@
 
-CREATE TABLE review (id BIGINT AUTO_INCREMENT NOT NULL, author VARCHAR(50) NULL, content VARCHAR(255) NULL, product_id BIGINT NULL, subject VARCHAR(255) NULL, CONSTRAINT PK_REVIEW PRIMARY KEY (id));
+CREATE TABLE review (id BIGINT AUTO_INCREMENT NOT NULL, author VARCHAR(50) NULL COMMENT 'The author of the Review', content VARCHAR(255) NULL COMMENT 'The content of the Review', subject VARCHAR(255) NULL COMMENT 'the Subject of the Review', product_id BIGINT NULL COMMENT 'The id of product this review belongs to', CONSTRAINT PK_REVIEW PRIMARY KEY (id)) COMMENT='Table to store the Reviews of our Products';
 
-ALTER TABLE review ADD CONSTRAINT ix_review_unique UNIQUE (product_id, id);
+ALTER TABLE review COMMENT = 'Table to store the Reviews of our Products';
 
-ALTER TABLE review ADD stars TINYINT NULL;
+ALTER TABLE review ADD UNIQUE (product_id, id);
+
+ALTER TABLE review ADD stars TINYINT NULL COMMENT 'number of stars from 1-5 the reviewer gives to the Product';
+
+ALTER TABLE review COMMENT = 'number of stars from 1-5 the reviewer gives to the Product';
 
