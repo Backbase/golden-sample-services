e063484dce9a323f6f0c5881a90c54e0c865f38b
Andrés Torres <towerspro@gmail.com>
2021-02-11 17:54:50 +0100

    bugfix: remove generated lib folder, fix path to the common types feature: add message to asserts in the tests

--- services/review/src/test/java/com/backbase/goldensample/review/v2/MapperTests.java
+++ services/review/src/test/java/com/backbase/goldensample/review/v2/MapperTests.java
@@ -16,7 +16,6 @@ class MapperTests {
 
     private final com.backbase.goldensample.review.v2.mapper.ReviewMapperV2 mapper = Mappers
         .getMapper(ReviewMapperV2.class);
-    ;
 
     @Test
     void mapperTests() {
@@ -24,26 +23,25 @@ class MapperTests {
         assertNotNull(mapper);
 
         Review review =
-            new Review().productId(1L).reviewId(1L).author("author").content("long content").subject("subject").stars(5)
-                .stars(5);
+            new Review().productId(1L).reviewId(1L).author("author").content("long content").subject("subject").stars(5);
 
         ReviewEntity entity = mapper.apiToEntity(review);
 
-        assertAll(() -> assertEquals(review.getProductId(), entity.getProductId()),
-            () -> assertEquals(review.getReviewId(), entity.getId()),
-            () -> assertEquals(review.getAuthor(), entity.getAuthor()),
-            () -> assertEquals(review.getSubject(), entity.getSubject()),
-            () -> assertEquals(review.getContent(), entity.getContent()),
-            () -> assertEquals(review.getStars(), entity.getStars()));
+        assertAll(() -> assertEquals(review.getProductId(), entity.getProductId(), "Product ID is different"),
+            () -> assertEquals(review.getReviewId(), entity.getId(), "Review ID is different"),
+            () -> assertEquals(review.getAuthor(), entity.getAuthor(), "Author is different"),
+            () -> assertEquals(review.getSubject(), entity.getSubject(), "Subject is different"),
+            () -> assertEquals(review.getContent(), entity.getContent(), "Content is different"),
+            () -> assertEquals(review.getStars(), entity.getStars(), "Stars is different"));
 
         Review api2 = mapper.entityToApi(entity);
 
-        assertAll(() -> assertEquals(review.getProductId(), api2.getProductId()),
-            () -> assertEquals(review.getReviewId(), api2.getReviewId()),
-            () -> assertEquals(review.getAuthor(), api2.getAuthor()),
-            () -> assertEquals(review.getSubject(), api2.getSubject()),
-            () -> assertEquals(review.getContent(), api2.getContent()),
-            () -> assertEquals(review.getStars(), api2.getStars()));
+        assertAll(() -> assertEquals(review.getProductId(), api2.getProductId(), "Product ID is different"),
+            () -> assertEquals(review.getReviewId(), api2.getReviewId(), "Review ID is different"),
+            () -> assertEquals(review.getAuthor(), api2.getAuthor(), "Author is different"),
+            () -> assertEquals(review.getSubject(), api2.getSubject(), "Subject is different"),
+            () -> assertEquals(review.getContent(), api2.getContent(), "Content is different"),
+            () -> assertEquals(review.getStars(), api2.getStars(), "Stars is different"));
     }
 
     @Test
@@ -60,23 +58,23 @@ class MapperTests {
 
         ReviewEntity entity = entityList.get(0);
 
-        assertAll(() -> assertEquals(review.getProductId(), entity.getProductId()),
-            () -> assertEquals(review.getReviewId(), entity.getId()),
-            () -> assertEquals(review.getAuthor(), entity.getAuthor()),
-            () -> assertEquals(review.getSubject(), entity.getSubject()),
-            () -> assertEquals(review.getStars(), entity.getStars()),
-            () -> assertEquals(review.getContent(), entity.getContent()));
+        assertAll(() -> assertEquals(review.getProductId(), entity.getProductId(), "Product ID is different"),
+            () -> assertEquals(review.getReviewId(), entity.getId(), "Review ID is different"),
+            () -> assertEquals(review.getAuthor(), entity.getAuthor(), "Author is different"),
+            () -> assertEquals(review.getSubject(), entity.getSubject(), "Subject is different"),
+            () -> assertEquals(review.getContent(), entity.getContent(), "Content is different"),
+            () -> assertEquals(review.getStars(), entity.getStars(), "Stars is different"));
 
         List<Review> api2List = mapper.entityListToApiList(entityList);
         assertEquals(apiList.size(), api2List.size());
 
         Review api2 = api2List.get(0);
 
-        assertAll(() -> assertEquals(review.getProductId(), api2.getProductId()),
-            () -> assertEquals(review.getReviewId(), api2.getReviewId()),
-            () -> assertEquals(review.getAuthor(), api2.getAuthor()),
-            () -> assertEquals(review.getSubject(), api2.getSubject()),
-            () -> assertEquals(review.getStars(), api2.getStars()),
-            () -> assertEquals(review.getContent(), api2.getContent()));
+        assertAll(() -> assertEquals(review.getProductId(), api2.getProductId(), "Product ID is different"),
+            () -> assertEquals(review.getReviewId(), api2.getReviewId(), "Review ID is different"),
+            () -> assertEquals(review.getAuthor(), api2.getAuthor(), "Author is different"),
+            () -> assertEquals(review.getSubject(), api2.getSubject(), "Subject is different"),
+            () -> assertEquals(review.getContent(), api2.getContent(), "Content is different"),
+            () -> assertEquals(review.getStars(), api2.getStars(), "Stars is different"));
     }
 }
